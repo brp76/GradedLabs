ff65e8f12127bba15f55ac92f519bc5d53a4ab15
Ben Posey <brp76@pitt.edu>
2015-11-05 12:46:30 -0500

    Deep copy implemented

--- src/Lab9.java
+++ src/Lab9.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
 public class Lab9 {
-
+	public static int swapCount = 0;
     /**
      * Helper method for printing out arrays.
      * @param int[] arr Array of integers to print
      */
     public static void printArray(int[] arr) {
-	System.out.print("[ ");
-	for (int j=0; j < (arr.length - 1); j++) {
-	    System.out.print(arr[j] + ", ");
-	}
-	if (arr.length > 0) {
-	    System.out.print(arr[arr.length - 1]);
-	}
-	System.out.println(" ]");
-	    
+		System.out.print("[ ");
+		for (int j=0; j < (arr.length - 1); j++) {
+		    System.out.print(arr[j] + ", ");
+		}
+		if (arr.length > 0) {
+		    System.out.print(arr[arr.length - 1]);
+		}
+		System.out.println(" ]");
+		    
     }
 
     /**
@@ -26,13 +26,14 @@ public class Lab9 {
      */
 
     public static void swap(int[] arr, int index1, int index2) {
-	if (index1 == index2) {
-	    // Do nothing!
-	} else {
-	    int tmp = arr[index1];
-	    arr[index1] = arr[index2];
-	    arr[index2] = tmp;
-	}
+		if (index1 == index2) {
+		    // Do nothing!
+		} else {
+		    int tmp = arr[index1];
+		    arr[index1] = arr[index2];
+		    arr[index2] = tmp;
+		    swapCount ++;
+		}
     }
 
     /**
@@ -41,40 +42,77 @@ public class Lab9 {
      */
     
     public static void selectionSort(int[] arr) {
-	
-	if (arr.length < 2) {
-	    return;
-	}
-	
-	int minIndex = 0;
-	int minVal = 0;
-
-	for (int j = 0; j < (arr.length-1); j++) {
-
-	    printArray(arr);
-	    minIndex = j;
-	    minVal = arr[j];
-	    for(int k = j + 1; k < arr.length; k++) {
-
-		if (arr[k] < minVal) {
-
-		    minVal = arr[k];
-		    minIndex = k;
+		swapCount = 0;
+		if (arr.length < 2) {
+		    return;
 		}
-	    }
-	    swap(arr, j, minIndex);
-	}
-
+		
+		int minIndex = 0;
+		int minVal = 0;
+	
+		for (int j = 0; j < (arr.length-1); j++) {
+	
+		    minIndex = j;
+		    minVal = arr[j];
+		    for(int k = j + 1; k < arr.length; k++) {
+	
+				if (arr[k] < minVal) {
+		
+				    minVal = arr[k];
+				    minIndex = k;
+				}
+		    }
+		    swap(arr, j, minIndex);
+		    
+		}
+		System.out.print("Selection sort: ");
+		printArray(arr);
+		System.out.println("Swaps = " + swapCount);
+    }
+    
+    public static void bubbleSort(int[] arr) {
+    	
+    }
+    
+    /**
+     * Creates deep copy of a1 array
+     * @param arr - original array
+     * @return - new array
+     */
+	public static int[][] deepCopy(int[][] arr) {
+    	int[][] newArr = new int[arr.length][arr[0].length];
+    	for (int i = 0; i < newArr.length; i++)
+    	     newArr[i] = Arrays.copyOf(arr[i], arr[i].length);
+    	return newArr;
     }
 
     public static void main(String[] args) {
-	int[][] a1 = { {8, 9, 5, 6, 4, 3},
-		       {9, 0, 14, 13, 10, 8, 2, 1, 17, 18, 19, 201, 220, 235, 2},
-		       {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200 },
-		       {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 1},
-		       {20, 18, 13, 12, 11, 9, 6, 5, 4, 3, 2, 1, -87, -900, -9, -909, -911, -80, -44, -32, -1000} };
-	
-
+		int[][] a1 = { {8, 9, 5, 6, 4, 3},
+			       {9, 0, 14, 13, 10, 8, 2, 1, 17, 18, 19, 201, 220, 235, 2},
+			       {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200 },
+			       {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 1},
+			       {20, 18, 13, 12, 11, 9, 6, 5, 4, 3, 2, 1, -87, -900, -9, -909, -911, -80, -44, -32, -1000} };
+		
+		
+		
+		int[][] a2 = deepCopy(a1);
+		for (int i = 0; i < a2.length; i ++) {
+			printArray(a2[i]);
+		}
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		selectionSort(a1[1]);
+		selectionSort(a1[2]);
+		selectionSort(a2[3]);
+		
+		System.out.println("A1: ");
+		for (int i = 0; i < a1.length; i ++) {
+			printArray(a1[i]);
+		}
+		
+		System.out.println("A2: ");
+		for (int i = 0; i < a2.length; i ++) {
+			printArray(a2[i]);
+		}
     }
 
     
